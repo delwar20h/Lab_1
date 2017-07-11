6b6af6f47d01ae0f8f2f746ead17335555217601
delwar20h <delwar20h@yahoo.com>
2017-07-11 00:22:06 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Input
+
+        Scanner scan = new Scanner(System.in);
+        double length;
+        double width;
+        double height;
+        double area;
+        double perimeter;
+        double volume;
+        String letterToContinue;
+
+        do {
+            System.out.print("Enter classroom length: ");
+            length = scan.nextDouble();
+            scan.nextLine();
+
+            System.out.print("Enter classroom width: ");
+            width = scan.nextDouble();
+            scan.nextLine();
+
+            System.out.print("Enter classroom height: ");
+            height = scan.nextDouble();
+            scan.nextLine();
+
+            // Processing
+            area = length * width;
+            perimeter = 2 * (length + width);
+            volume = length * width * height;
+
+            // Output
+            System.out.println("Area of your classroom is: " + area);
+            System.out.println("Perimeter of your classroom is: " + perimeter);
+            System.out.println("Volume of your classroom is: " + volume);
+            System.out.println();
+
+            System.out.println("Would you like to continue? Y/N ");
+
+            letterToContinue = scan.next().toUpperCase();
+            if (!letterToContinue.equals("Y") || (!letterToContinue.equals("N"))) {
+                System.out.println("Invalid entry.");
+            }
+
+        } while (letterToContinue.equals("Y"));
+
+
+    }
+}
